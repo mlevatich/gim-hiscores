f12e7dc9821f3178c755d405b25a106691bcf39b
Max Levatich <ml4553@columbia.edu>
2024-12-31 09:29:36 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# OSRS Group Ironman boss hiscores
+
+This is a Python script to scrape the Old School Runescape Group Ironman hiscores, in order to generate custom boss hiscores that the OSRS website does not provide.
+
+Features:
+- Rank group ironmen groups by boss killcounts!
+- Filter ranking to a certain group size
+- Filter ranking to only prestiged groups
+- Filter ranking to only hardcore groups
+- Cache a copy of hiscores locally
+
+Example: Ranking Theatre of Blood completions by prestiged duo group ironmen
+```
+# As of 12/31/24
+> python gim_hiscores.py 25 "Theatre of Blood" -size 2 -prestige
+
+Retrieved first 0 pages from cache.
+Scraping uncached pages 1-25 from hiscores... *************************
+Total: 234 groups.
+Retrieved members of 0 groups from cache.
+Scraping group members for 234 uncached groups...
+Total: 468 players.
+Retrieved scores for 0 players from cache.
+Scraping killcount for 468 uncached players...
+
+
+Prestige Group Ironman Hiscores (group size: 2)
+Theatre of Blood
+
+Group           Killcount
+
+zulufanclub1    52
+iron berg       50
+```
+
+# Why is this difficult?
+
+Jagex does not provide a hiscores API and the web-based group ironmen hiscores are very limited. In order to determine a ranking of group boss killcounts, several web requests must be made:
+- One per page of the group ironman hiscores to find group names for the desired size.
+- One per group name to find the members of the group.
+- One per group member to tally up their killcounts.
+
+This can add up to thousands of requests, which is a) very slow and b) results in your IP address being blocked by Jagex's servers. This imposes some limitations on the tool and requires some creative design:
+- Web requests are multi-threaded with a default pool size of 20 for speed.
+- A local cache of the hiscores is built as the tool runs, so that when your IP address is inevitably blocked, you can resume later by reading from the cache (over time, the cache will become out-of-date as the hiscores change, and should be deleted).
+- BeautifulSoup is used to parse the raw html output.
+- Some amount of browser impersonation is used via `curl_cffi`, though whether this has any effect is debateable.
+
+# Installation
+
+```
+git clone
+cd gim-hiscores
+pip install beautifulsoup4
+pip install curl-cffi --upgrade
+```
+
+# Usage guide
+
+```
+usage: gim_hiscores.py [-h] [-size SIZE] [-hardcore] [-prestige] [-pool POOL] [-delete-cache] pages activity
+
+positional arguments:
+  pages          How many pages of groups to include from the hiscores
+  activity       Name of boss or activity as it appears on hiscores
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -size SIZE     Group size (default: 2)
+  -hardcore      Filter to only living hardcore groups
+  -prestige      Filter to only prestiged groups
+  -pool POOL     Number of threads to make requests to hiscores (default: 20)
+  -delete-cache  Refresh local copy of hiscores (you will be IP blocked as the cache regenerates)
+```
+
+For large requests or when making many repeat requests, **your IP address WILL be blocked by Jagex's servers**. I don't know a way around this in pure Python. All of the results you received before the IP block are cached locally. Simply wait for awhile (15 minutes to an hour in my experience), and then re-run your command to make progress - the program will use the cached results to avoid duplicating old requests.
---
+++ gim_hiscores.py
@@ -0,0 +1,265 @@
+import argparse
+from bs4 import BeautifulSoup
+from curl_cffi import requests
+from multiprocessing import Pool
+import json
+import sys
+import os
+
+# TODO: test: hardcore, size 5, different bosses
+
+# Filenames for storing local copies of hiscores
+cache_files = [
+    "local_hs_2.json",
+    "local_hs_hc_2.json",
+    "local_hs_3.json",
+    "local_hs_hc_3.json",
+    "local_hs_4.json",
+    "local_hs_hc_4.json",
+    "local_hs_5.json",
+    "local_hs_hc_5.json",
+    "local_groups.json",
+    "players.json"
+]
+
+# Error message for IP block
+scrape_err = "your IP was blocked while scraping, but some results were cached - continue by re-running later"
+
+def die(msg):
+    print("gim_hiscores.py: error: {}".format(msg))
+    exit(1)
+
+def parse_args():
+    p = argparse.ArgumentParser()
+    p.add_argument("pages", type=int, help="How many pages of groups to include from the hiscores")
+    p.add_argument("activity", help="Name of boss or activity as it appears on hiscores")
+    p.add_argument("-size", type=int, default=2, help="Group size (default: 2)")
+    p.add_argument("-hardcore", action="store_true", default=False, help="Filter to only living hardcore groups")
+    p.add_argument("-prestige", action="store_true", default=False, help="Filter to only prestiged groups")
+    p.add_argument("-pool", type=int, default=20, help="Number of threads to make requests to hiscores (default: 20)")
+    p.add_argument("-delete-cache", action="store_true", default=False, help="Refresh local copy of hiscores (you will be IP blocked as the cache regenerates)")
+    args = p.parse_args()
+    if args.hardcore and args.prestige:
+        die("'-hardcore' and '-prestige' cannot both be set, as the hardcore hiscores don't track prestige")
+    if args.pool < 1:
+        die("number of threads for '-pool' must be >= 1")
+    return args
+
+# Wrapper for making a request and printing some progress
+# (response may be an IP block or connection error)
+def request(url, params=None):
+    try:
+        r = requests.get(url, impersonate='chrome', params=params)
+        print("*", end='') # indicates progress
+        sys.stdout.flush()
+        return r.text
+    except ConnectionError:
+        return ""
+
+# Request a page of the group ironman hiscores
+def page_request(hc, size, page):
+    url_base = 'https://secure.runescape.com/m=hiscore_oldschool{}_ironman/group-ironman/?groupSize={}&page={}'
+    url = url_base.format("_hardcore" if hc else "", size, page)
+    return request(url)
+
+# Request the page for a particular group, in order to get its members
+def group_request(g):
+    url = 'https://secure.runescape.com/m=hiscore_oldschool_ironman/group-ironman/view-group'
+    params = { 'name': g.replace(chr(160), ' ') }
+    return request(url, params=params)
+
+# Request the JSON index of a set of players' hiscores
+def kc_request(ms):
+    responses = []
+    url = 'https://services.runescape.com/m=hiscore_oldschool/index_lite.json'
+    for m in ms:
+        params = { 'player': m.replace(chr(160), ' ') }
+        r = request(url, params=params)
+        responses.append(r)
+    return responses
+
+def fetch_groups(size, hc, prestige, pages, threads):
+    gs = []
+
+    # Retrieve pages from local cache
+    cache_file = "gim_hiscores_cache/local_hs{}_{}.json".format("_hc" if hc else "", size)
+    with open(cache_file, 'r') as f: cache = json.load(f)
+    for page in cache[:pages]:
+        for row in page:
+            if not ((prestige and not row['prestiged']) or (hc and row['dead'])):
+                gs.append(row['name'])
+    start = len(cache) + 1
+    print("Retrieved first {} pages from cache.".format(min(len(cache), pages)))
+    if start <= pages: print("Scraping uncached pages {}-{} from hiscores... ".format(start, pages), end='')
+    else:              print("No pages scraped from hiscores.", end='')
+
+    # Scrape any pages which were not found in the cache
+    # Multi-thread requests to speed things up
+    with Pool(threads) as p:
+        page_responses = p.starmap(page_request, list(zip([hc] * pages, [size] * pages, range(start, pages + 1))))
+    print("")
+
+    # Parse each html response with beautifulsoup
+    for pr in page_responses:
+        soup = BeautifulSoup(pr, features="html.parser")
+        isprestige = lambda e: bool(e.find_previous_sibling('img', title='Group Prestiged'))
+        isdead = lambda e: 'uc-scroll__table-row--type-death' in e.parent.parent['class']
+        invalid = lambda e: (prestige and not isprestige(e)) or (hc and isdead(e))
+        candidates = soup.find_all('a', attrs={'class': 'uc-scroll__link'})
+
+        # Less than 20 results indicates we were IP blocked
+        if len(candidates) != 20:
+            with open(cache_file, 'w') as f: json.dump(cache, f)
+            die(scrape_err)
+
+        # If a valid page was scraped, update the set of groups and the cache
+        gs.extend([str(e.string) for e in candidates if not invalid(e)])
+        cache.append([{'name': str(e.string), 'dead': isdead(e), 'prestiged': isprestige(e)} for e in candidates])
+    
+    # Write to cache before returning
+    with open(cache_file, 'w') as f: json.dump(cache, f)
+    print("Total: {} groups.".format(len(gs)))
+    return gs
+
+def fetch_members(gs, size, threads):
+    all_members = {}
+
+    # Retrieve group members from local cache
+    cache_file = "gim_hiscores_cache/local_groups.json"
+    with open(cache_file, 'r') as f: cache = json.load(f)
+    uncached_gs = []
+    for g in gs:
+        if g not in cache:          uncached_gs.append(g)
+        elif len(cache[g]) == size: all_members[g] = cache[g]
+    print("Retrieved members of {} groups from cache.".format(len(all_members)))
+    if len(uncached_gs) > 0: print("Scraping group members for {} uncached groups... ".format(len(uncached_gs)), end='')
+    else:                    print("No groups scraped.", end='')
+
+    # Scrape for members of any groups which were not found in the cache
+    # Multi-thread requests to speed things up
+    with Pool(threads) as p:
+        group_responses = p.map(group_request, uncached_gs)
+    print("")
+
+    # Parse each html response with beautifulsoup
+    for (g, gr) in zip(uncached_gs, group_responses):
+        soup = BeautifulSoup(gr, features="html.parser")
+        members = [str(e.string) for e in soup.find_all('a', attrs={'class': 'uc-scroll__link'})]
+
+        # If the number of members scraped is less than the group size, we got IP blocked
+        # (or the group can't be indexed by name because it was never set, 
+        # which is rare but possible in the top groups. We just exclude those.)
+        if len(members) != size:
+            if g == "Group name not set":
+                cache[g] = []
+                continue
+            with open(cache_file, 'w') as f: json.dump(cache, f)
+            die(scrape_err)
+
+        # Otherwise update the group-to-members mapping and the cache
+        all_members[g] = members
+        cache[g] = members
+
+    # Write to cache before returning
+    with open(cache_file, 'w') as f: json.dump(cache, f)
+    print("Total: {} players.".format(size * len(gs)))
+    return all_members
+
+def fetch_ranks(all_members, boss, threads):
+    scores = []
+    all_members_list = all_members.items()
+
+    # Pull from cache, and separate out what goes to pool
+    # Only take cache values where all players in a group were cached, to make
+    # things easy
+    cache_file = "gim_hiscores_cache/players.json"
+    with open(cache_file, 'r') as f: cache = json.load(f)
+    uncached_members = []
+    found = 0
+    left = 0
+    for (g, ms) in all_members_list:
+        if all([m in cache for m in ms]):
+            scores.append((g, sum([max(0, int(cache[m][boss])) for m in ms if boss in cache[m]])))
+            found += len(ms)
+        else:
+            uncached_members.append((g, ms))
+            left += len(ms)
+    print("Retrieved scores for {} players from cache.".format(found))
+
+    # Scrape boss kcs for each group and group member using the JSON API
+    # Multi-thread requests to speed things up
+    if left > 0:
+        print("Scraping killcount for {} uncached players... ".format(left), end='')
+    else:
+        print("No player scores scraped.", end='')
+    with Pool(threads) as p:
+        kc_responses = p.map(kc_request, [e[1] for e in uncached_members])
+    print("")
+
+    # For each group, load JSON for each group member
+    # and sum the kc ('score') for the requested activity
+    for g, kcrs in zip([e[0] for e in uncached_members], kc_responses):
+        kc = 0
+        for m, kcr in zip(all_members[g], kcrs):
+
+            # If the json load fails, we got IP blocked (or the player is missing)
+            try:
+                js = json.loads(kcr)
+            except:  
+                if '<title>404 - Page not found</title>' in kcr:
+                    print("WARNING: player not found on hiscores: {}".format(m))
+                    cache[m] = {}
+                    continue
+                else:
+                    with open(cache_file, 'w') as f: json.dump(cache, f)
+                    die(scrape_err)
+
+            # Increment activity kc for this member
+            score_mb = [x['score'] for x in js['activities'] if x['name'] == boss]
+            if len(score_mb) == 0:
+                die("the provided activity '{}' does not exist on the hiscores".format(boss))
+            kc += max(0, int(score_mb[0]))
+
+            # Update cache
+            cache[m] = {}
+            for a in js['activities']: cache[m][a['name']] = a['score']
+        scores.append((g, kc))
+    with open(cache_file, 'w') as f: json.dump(cache, f)
+    print("\n")
+
+    # Sort groups by group kc, filtering out 0kc groups
+    ranking = sorted(scores, key = lambda s: s[1], reverse = True)
+    ranking = [ (r, kc) for (r, kc) in ranking if kc > 0 ]
+    return ranking
+
+# Pretty print the rankings when we're all done
+def dump_ranking(args, ranking):
+    ps = "Prestige " if args.prestige else ""
+    hs = "Hardcore " if args.hardcore else ""
+    header = "{}{}Group Ironman Hiscores (group size: {})".format(ps, hs, args.size)
+    ranks = "\n".join(["{}{}".format(g.ljust(16), kc) for (g, kc) in ranking])
+    cols = "Group           Killcount"
+    print(header + "\n" + args.activity + "\n\n" + cols + "\n\n" + ranks)
+
+# Delete all locally saved hiscores and groups and regenerate files
+def new_cache():
+    if not os.path.exists('gim_hiscores_cache'):
+        os.mkdir('gim_hiscores_cache')
+    for cf in cache_files:
+        with open('gim_hiscores_cache/' + cf, 'w') as f:
+            if cf == "local_groups.json" or cf == "players.json":
+                json.dump({}, f)
+            else:
+                json.dump([], f)
+
+# Entry point
+def main():
+    args = parse_args()
+    if args.delete_cache or not os.path.exists('gim_hiscores_cache'): new_cache()
+    gs = fetch_groups(args.size, args.hardcore, args.prestige, args.pages, args.pool)
+    members = fetch_members(gs, args.size, args.pool)
+    ranking = fetch_ranks(members, args.activity, args.pool)
+    dump_ranking(args, ranking)
+
+if __name__ == "__main__":
+    main()
